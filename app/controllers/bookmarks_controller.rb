f6bdc28554387c7d09997fb4c818abebcae34130
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-24 19:47:11 +0100

    updated bookmarks controller; added new bookmark feature in controller and view; updated list show to include movie list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,16 @@
 class BookmarksController < ApplicationController
+
+  before_action :find_list
+
   def new
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
     end
@@ -18,6 +22,10 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
   end
 end
