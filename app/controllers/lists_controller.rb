acfee7617f68d6ba3a057f6e8b69a3d4eaa266b9
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-26 20:43:08 +0100

    updated model relationship between list and bookmarks; updated index with new colors and style; added list destroy action and route in index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def index
-    @lists = List.all
+    @lists = List.order(:name)
   end
 
   def show
@@ -18,6 +18,12 @@ class ListsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to root_path
   end
 
   private
