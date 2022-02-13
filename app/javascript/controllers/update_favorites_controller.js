a8d3a3930bfbe70e4d46681e6a0a551f2cb0e9f2
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-13 03:32:34 +0100

    added update favorites feature via stimulus

--- app/javascript/controllers/update_favorites_controller.js
+++ app/javascript/controllers/update_favorites_controller.js
@@ -1,9 +1,35 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ "form" ];
+  static targets = [ "form", 'movieId', 'delete', 'btnForm'];
 
-  favorite() {
+  favorite(e) {
     console.log("Entered my AJAX method")
+    const movieId = this.movieIdTarget.value
+    const url = "/movies/popular";
+
+    fetch(url, {
+      method: "GET",
+      headers: { Accept: "application/json" },
+    }).then((response) => response.json())
+      .then(data => {
+        const favorite = data.favorites.find((favorite) => favorite.movie_id == movieId);
+
+        if (favorite) {
+          this.formTarget.action = `/favorites/${favorite.id}`
+          this.movieIdTarget.id = favorite.id
+
+          this.btnFormTarget.textContent = 'Remove from favorites'
+          this.deleteTarget.name = '_method'
+          this.deleteTarget.value = 'delete'
+
+        } else {
+          this.formTarget.action = `/favorites`;
+          this.btnFormTarget.textContent = 'Add to favorites'
+          this.movieIdTarget.value = movieId;
+          this.deleteTarget.name = ''
+          this.deleteTarget.value = ''
+        }
+      });
+    }
   }
-}
