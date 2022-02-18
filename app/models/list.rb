1da2679cbb154921915fdb8e85cf1fc292e9df6e
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-18 19:03:46 +0100

    updated list validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :bookmarks
   has_many :movies, through: :bookmarks
-  validates :name, presence: true, uniqueness: { scope: user }
+  validates :name, presence: true, uniqueness: { scope: :user }
   has_one_attached :photo
 end
