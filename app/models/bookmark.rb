159fbf26a94bf558335267c38e6026feed8126aa
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-18 18:58:23 +0100

    model validations update

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
-  belongs_to :list, dependent: :destroy
+  belongs_to :list
 
   validates :movie, presence: true, uniqueness: { scope: :list }
   validates :comment, presence: true, length: { minimum: 6 }
