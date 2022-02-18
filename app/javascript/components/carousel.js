f242245fc953360f43d453029c074dcd151732c2
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-19 00:21:03 +0100

    added responsiveness to swiper

--- app/javascript/components/carousel.js
+++ app/javascript/components/carousel.js
@@ -6,14 +6,34 @@ Swiper.use([Navigation, Pagination]);
 
 const loadSwiperCarousel = () => {
   new Swiper(".mySwiper", {
-    slidesPerView: 9,
-    spaceBetween: 10,
     slidesPerGroup: 2,
     loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+      // when window width is >= 800px
+      800: {
+        slidesPerView: 9,
+        spaceBetween: 10,
+      }
+    }
   });
 }
 
