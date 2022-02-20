e2c8215987981d237e91da29538a7e03ced78b7e
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-20 16:52:17 +0100

    added liked field in likes table

---
+++ db/migrate/20220220155139_add_liked_to_likes.rb
@@ -0,0 +1,5 @@
+class AddLikedToLikes < ActiveRecord::Migration[6.1]
+  def change
+    add_column :likes, :liked, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_154509) do
+ActiveRecord::Schema.define(version: 2022_02_20_155139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_02_20_154509) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "liked"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
