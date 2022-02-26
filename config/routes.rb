3d7399acd637dfe235d345025796c2f259b4b8be
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-26 23:46:01 +0100

    removed lists and bookmarks routes and links from navbar; updated favorites layout to grid

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,6 @@ Rails.application.routes.draw do
   delete '/favorites', to: 'favorites#delete_by_movie_id'
   resources :likes, only: %i[index create update destroy]
 
-  resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create]
-    collection do
-      get '/featured', to: 'lists#feature'
-    end
-  end
-  resources :bookmarks, only: [:destroy]
-  resources :lists, only: [:destroy]
   resources :movies, only: %i[index] do
     collection do
       get '/discover', to: 'movies#discover'
